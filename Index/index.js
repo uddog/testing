f8577ed34c6051448571840180499944c4969909
uddog <137387883+uddog@users.noreply.github.com>
2024-02-06 10:32:45 +0600

    Add files via upload

---
+++ Index/index.js
@@ -0,0 +1,254 @@
+const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT5yPBQL7OkwisJJ6Dq4jpzATrchx3wbyxQQ09mH0BoPrTFr8FYnKxkT7xjvWB8P51Gled65w6S8VQH/pub?output=csv&gid=0';
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// img slider
+async function fetchImageSources() {
+    const data = await fetchData(csvUrl);
+
+    if (data) {
+        const rows = data.split('\n').map(row => row.split(','));
+
+        const imageSlider = document.getElementById("imageSlider");
+        const imgDataArray = rows.slice(1).map(row => ({
+            src: row[0].trim(),
+            href: row[1] ? row[1].trim() : null
+        })).filter(imgData => imgData.src !== "");
+
+        imgDataArray.forEach(imgData => {
+            const imgElement = document.createElement("img");
+
+            if (imgData.href) {
+                imgElement.setAttribute("data-href", imgData.href);
+                imgElement.addEventListener("click", handleImgClick);
+                imgElement.style.cursor = "pointer";
+            }
+
+            imgElement.src = imgData.src;
+            imageSlider.appendChild(imgElement);
+        });
+
+        initImageSlider();
+    }
+}
+function handleImgClick() {
+    const href = this.getAttribute("data-href");
+    if (href) {
+        window.location.href = href;
+    }
+}
+
+
+
+let touchStartX = 0;
+let touchEndX = 0;
+let isTouching = false;
+
+function handleTouchStart(event) {
+    isTouching = true;
+    touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchEnd(event) {
+    isTouching = false;
+    touchEndX = event.changedTouches[0].clientX;
+    handleSwipe();
+}
+
+function handleSwipe() {
+    const deltaX = touchEndX - touchStartX;
+    const sensitivity = 50; // Adjust this value as needed
+
+    if (deltaX > sensitivity) {
+        // Swipe right, move to the previous slide
+        pauseSlider();
+        slide(-1);
+        resumeSlider();
+    } else if (deltaX < -sensitivity) {
+        // Swipe left, move to the next slide
+        pauseSlider();
+        slide(1);
+        resumeSlider();
+    }
+}
+
+let sliderInterval;
+let counter = 0;
+
+function initImageSlider() {
+    const sliderContainer = document.getElementById("imageSliderContainer");
+    const slider = document.getElementById("imageSlider");
+
+    sliderContainer.addEventListener("touchstart", handleTouchStart);
+    sliderContainer.addEventListener("touchend", handleTouchEnd);
+
+    sliderInterval = setInterval(() => {
+        if (!isTouching) {
+            slide(1);
+        }
+    }, 4000);  //time
+}
+
+function slide(direction) {
+    if (direction === 1 && counter >= imageSlider.children.length - 1) {
+        counter = 0;
+    } else if (direction === -1 && counter <= 0) {
+        counter = imageSlider.children.length - 1;
+    } else {
+        counter += direction;
+    }
+
+    imageSlider.style.transition = "transform 0.5s ease-in-out";
+    imageSlider.style.transform = `translateX(${-counter * 100}%)`;
+}
+
+function pauseSlider() {
+    clearInterval(sliderInterval);
+}
+
+function resumeSlider() {
+    sliderInterval = setInterval(() => {
+        if (!isTouching) {
+            slide(1);
+        }
+    }, 4000);
+}
+
+
+
+
+
+
+// 4 box
+async function fetchProfileData() {
+    const data = await fetchData(csvUrl);
+
+    if (data) {
+        const rows = data.split('\n').map(row => row.split(','));
+
+        const profileSection = document.getElementById("profileSection");
+
+        rows.slice(1).forEach(row => {
+            const isProfileEmpty = row.slice(2, 5).every(cell => cell.trim() === '');
+
+            if (!isProfileEmpty) {
+                const profileDiv = document.createElement("div");
+                profileDiv.classList.add("profile");
+
+                const hrefValue = row[5].trim();
+                if (hrefValue !== '') {
+                    const profileLink = document.createElement("a");
+                    profileLink.href = hrefValue;
+
+                    if (row[2].trim() !== '') {
+                        const profileImage = document.createElement("img");
+                        profileImage.src = row[2].trim();
+                        profileLink.appendChild(profileImage);
+                    }
+
+                    if (row[3].trim() !== '') {
+                        const profileName = document.createElement("h3");
+                        profileName.textContent = row[3].trim();
+                        profileLink.appendChild(profileName);
+                    }
+
+                    if (row[4].trim() !== '') {
+                        const profileDetails = document.createElement("p");
+                        profileDetails.textContent = row[4].trim();
+                        profileLink.appendChild(profileDetails);
+
+                        profileDiv.appendChild(profileLink);
+                        profileSection.appendChild(profileDiv);
+                    }
+                } else {
+                    if (row[2].trim() !== '') {
+                        const profileImage = document.createElement("img");
+                        profileImage.src = row[2].trim();
+                        profileDiv.appendChild(profileImage);
+                    }
+
+                    if (row[3].trim() !== '') {
+                        const profileName = document.createElement("h3");
+                        profileName.textContent = row[3].trim();
+                        profileDiv.appendChild(profileName);
+                    }
+
+                    if (row[4].trim() !== '') {
+                        const profileDetails = document.createElement("p");
+                        profileDetails.textContent = row[4].trim();
+                        profileDiv.appendChild(profileDetails);
+                    }
+
+                    profileSection.appendChild(profileDiv);
+                }
+            }
+        });
+    }
+}
+
+//for post
+async function fetchAndDisplayData() {
+
+    const csvPost = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT5yPBQL7OkwisJJ6Dq4jpzATrchx3wbyxQQ09mH0BoPrTFr8FYnKxkT7xjvWB8P51Gled65w6S8VQH/pub?output=csv&gid=2089505816';
+
+    const data = await fetchData(csvPost);
+
+    if (data) {
+        const rows = data.split('\n');
+        const profilesContainer = document.getElementById('csvData');
+        const startIndex = Math.max(0, rows.length - 2);
+
+        for (let i = startIndex; i < rows.length; i++) {
+            const columns = rows[i].split(',');
+
+            const profileImageSrc = columns[0].trim();
+            if (profileImageSrc) {
+                const profileDiv = document.createElement('div');
+                profileDiv.classList.add('threecontent');
+
+                const profileImage = document.createElement('img');
+                profileImage.src = profileImageSrc;
+                profileImage.alt = 'Profile Image';
+                profileDiv.appendChild(profileImage);
+
+                const nameElement = document.createElement('h3');
+                nameElement.textContent = columns[1].trim();
+                profileDiv.appendChild(nameElement);
+
+                for (let j = 2; j < columns.length; j++) {
+                    const columnContent = columns[j].trim();
+                    if (columnContent && j !== 4) {  // Skip column E (index 4)
+                        const element = document.createElement(j === 3 ? 'button' : 'p');
+                        if (j === 3) {
+                            element.textContent = 'Details';
+                            element.addEventListener('click', function () {
+                                localStorage.setItem('PostNo', i);
+                                window.location.href = columnContent;
+                            });
+                        } else {
+                            element.textContent = columnContent;
+                        }
+                        profileDiv.appendChild(element);
+                    }
+                }
+                
+
+                profilesContainer.appendChild(profileDiv);
+            }
+        }
+    }
+}
+
+
+// Call the functions
+fetchImageSources();
+fetchProfileData();
+fetchAndDisplayData();
---
+++ Navbar/navbar.js
@@ -0,0 +1,178 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const savedRowIndex = localStorage.getItem('rowIndex');
+    const savedUserPassword = localStorage.getItem('userPassword'); // Added line to get userPassword from localStorage
+    const sidebarTop = document.getElementById('sidebarTop');
+    const homeLink = document.getElementById('homeLink');
+    const sidebarButton = document.getElementById('sidebarButton');
+    const sidebar = document.getElementById('sidebar');
+
+    const navbar = document.getElementById('navbar');
+    const profileBtn = document.getElementById('profileBtn');
+    const logoutBtn = document.getElementById('logoutBtn');
+
+    if (savedRowIndex && savedUserPassword) {
+        displayUserProfile(savedRowIndex, savedUserPassword, navbar); // Pass user password to displayUserProfile
+        profileBtn.style.display = 'block';
+    } else {
+        // If rowIndex or userPassword is not found, hide the entire sidebar
+        sidebar.style.display = 'none';
+        sidebarButton.style.display = 'none'; // Hide the sidebar button
+        navbar.innerHTML = '<ul><li><a href="login.html">Login</a></li></ul>';
+        console.error('Row index or user password not found in localStorage.');
+        profileBtn.style.display = 'none';
+        logoutBtn.style.display = 'none';
+    }
+});
+
+async function displayUserProfile(rowIndex, userPassword, navbar) {
+    const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRDv7MQUbN2J6k-mj5UI9RsITtNmwhR1LwkxBc3tQ_aUU984nUy1aEIQIWgVUeOJyKTDVkrggQBqn7n/pub?output=csv&gid=0';
+
+    try {
+        const response = await fetch(csvUrl);
+        const data = await response.text();
+        const rows = data.split('\n').map(row => row.split(','));
+
+        if (rowIndex >= 1 && rowIndex < rows.length) {
+            const userRow = rows[rowIndex];
+            const storedUserPassword = userRow[26].trim(); // Assuming column AA is at index 26
+
+            // Check if the stored user password matches the one in the spreadsheet
+            if (userPassword !== storedUserPassword) {
+                // If passwords don't match, remove rowIndex from localStorage and redirect to login page
+                localStorage.removeItem('rowIndex');
+                localStorage.removeItem('userName');
+                localStorage.removeItem('userPassword');
+                location.href = 'index.html';
+                return;
+            }
+
+            // The rest of your existing code for displaying user profile
+            const photoUrl = userRow[18];
+            navbar.innerHTML = `<ul><li><img src="${photoUrl}" alt="User Photo" onclick="toggleBio()"></li></ul>`;
+
+            const bioSection = document.getElementById('bioSection');
+            const userName = userRow[4];
+            const userUsername= userRow[1];
+            const bloodGroup = userRow[9].trim();
+            const address = userRow[11].trim();
+            const balance = userRow[20].trim();
+            const additionalValue = userRow[27].trim(); // Assuming column AB is at index 27
+            const roleValue = userRow[15].trim(); // Assuming column P is at index 15
+            const birthdate = userRow[8].trim(); 
+            const school = userRow[12].trim();
+
+
+          
+            //saving in local storage
+            localStorage.setItem('userUsername', userUsername);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('userBloodGroup', bloodGroup);
+            localStorage.setItem('userAddress', address);
+            localStorage.setItem('userSchool', school);
+            localStorage.setItem('userBirthDay', birthdate);
+            localStorage.setItem('userProfession', roleValue);
+
+
+            bioSection.innerHTML = `<p>${userName}</p><p>Blood Group: ${bloodGroup}</p><p>Address: ${address}</p><p>Total Donate ${balance}</p>`;
+
+            // Check the value in column AB and show the corresponding Admin button(s)
+            switch (additionalValue) {
+                case '0':
+                    addAdminButton('Search', '/admin0.html'); // Set the link for Admin 1
+                    addAdminButton('Udrita', 'https://kilt-bluefish.cyclic.app/'); // Set the link for Admin 1
+                    break;
+                case '1':
+                    addAdminButton('Search', '/admin0.html'); // Set the link for Admin 1
+                    addAdminButton('Access', '/admin1.html'); // Set the link for Admin 1
+                    addAdminButton('Udrita', 'https://kilt-bluefish.cyclic.app/'); // Set the link for Admin 1
+                    break;
+                case '2':
+                    addAdminButton('Search', '/admin0.html'); // Set the link for Admin 1
+                    addAdminButton('Access Pro', '/admin2.html'); // Set the link for Admin 2
+                    addAdminButton('Access', 'admin1.html'); // Set the link for Admin 1
+                    addAdminButton('Udrita', 'https://kilt-bluefish.cyclic.app/'); // Set the link for Admin 1
+                    break;
+                 case '3':
+                    addAdminButton('Search', '/admin0.html'); // Set the link for Admin 1
+                    addAdminButton('Access Pro', '/admin2.html'); // Set the link for Admin 2
+                    addAdminButton('Access', '/admin1.html'); // Set the link for Admin 1
+                    addAdminButton('Admin', '/admin3.html'); // Set the link for Admin 1
+                    addAdminButton('Udrita', 'https://kilt-bluefish.cyclic.app/'); // Set the link for Admin 1
+                    break;
+                // Add more cases as needed
+                default:
+                    break;
+            }
+            switch (roleValue) {
+                default:
+                    addAdminButton(roleValue,'/Profession/profession.html'); // Use roleValue as button name and link to profession.html
+                    break;
+            }
+            
+        } else {
+            navbar.innerHTML = '<ul><li><a href="login.html">Login</a></li></ul>';
+            console.error('Invalid row index:', rowIndex);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function addAdminButton(adminButtonText, adminButtonLink) {
+    const sidebar = document.getElementById('sidebar');
+    const adminButton = document.createElement('a');
+    adminButton.href = adminButtonLink; // Set the href attribute for the link
+    adminButton.textContent = adminButtonText;
+    adminButton.onclick = adminButtonClick;
+
+    // Insert the Admin button after the "Home" link
+    const homeLink = document.getElementById('homeLink');
+    if (homeLink) {
+        sidebar.insertBefore(adminButton, homeLink.nextSibling);
+    }
+}
+
+function adminButtonClick() {
+    // Add your admin button click logic here
+    console.log('Admin button clicked');
+}
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.width = sidebar.style.width === '40vw' ? '0' : '40vw';
+}
+
+function toggleBio() {
+    const bioSection = document.getElementById('bioSection');
+    bioSection.style.display = (bioSection.style.display === 'block') ? 'none' : 'block';
+}
+
+function openLogoutPopup() {
+    const overlay = document.getElementById('overlay');
+    const logoutPopup = document.getElementById('logoutPopup');
+    overlay.style.display = 'block';
+    logoutPopup.style.display = 'block';
+}
+
+function closeLogoutPopup() {
+    const overlay = document.getElementById('overlay');
+    const logoutPopup = document.getElementById('logoutPopup');
+    overlay.style.display = 'none';
+    logoutPopup.style.display = 'none';
+}
+
+function logout() {
+    
+    localStorage.removeItem('rowIndex');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userBloodGroup');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userAddress');
+    localStorage.removeItem('userBirthDay');
+    localStorage.removeItem('userProfession');
+    
+    localStorage.removeItem('mainColor');
+    location.reload();
+}
+
+
